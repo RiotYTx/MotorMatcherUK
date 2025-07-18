63825db15c845cb87c979a0681b5db2302788b17
RiotYTx <117189478+RiotYTx@users.noreply.github.com>
2025-07-18 16:11:54 +0000

    added testing

--- server.js
+++ server.js
@@ -69,4 +69,10 @@ app.get("/cars", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
- 
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+}
+
+module.exports = app;
